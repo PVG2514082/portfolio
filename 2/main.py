9e5ba59cf680d037c23a9f56d494e107e385514f
PVG2514082 <118740743+PVG2514082@users.noreply.github.com>
2023-04-16 21:42:45 +0300

    Add files via upload

--- 2/main.py
+++ 2/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import sqlite3
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +10,6 @@ def index():
 @app.route('/search', methods=['GET'])
 def search():
     search_term = request.args.get('search')
-
     return render_template('search_results.html', search_term=search_term)
 
 @app.route('/resume', methods=['GET', 'POST'])
@@ -22,6 +23,31 @@ def create_resume():
         return render_template('resume_confirmation.html', name=name)
     else:
         return render_template('create_resume.html')
+
+@app.route('/authorization', methods=['GET', 'POST'])
+def form_authorization():
+    if request.method == 'POST':
+        Login = request.form.get('Login')
+        Password = request.form.get('Password')
+
+        db_lp = sqlite3.connect('login_password.db')
+        cursor_db = db_lp.cursor()
+        cursor_db.execute(('''SELECT password FROM login_passwords.db
+                                                   WHERE login = '{}';
+                                                   ''').format(Login))
+        pas = cursor_db.fetchall()
+
+        cursor_db.close()
+        try:
+            if pas[0][0] != Password:
+                return render_template('auth_bad.html')
+        except:
+            return render_template('auth_bad.html')
+
+        db_lp.close()
+        return render_template('successfulauth.html')
+
+    return render_template('authorization.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
